d1b0377c1cc9e0dee26bf63becc2e669b1f693e7
Aytam Asadova <aytanasadova1993@gmail.com>
2022-11-08 22:48:43 +0100

    clearing

--- src/main/java/xm/cryptorecommendation/domain/ErrorResponse.java
+++ src/main/java/xm/cryptorecommendation/domain/ErrorResponse.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class ErrorResponse {
 
 
-    private String message;
-    private List<String> details;
+    private final String message;
+    private final List<String> details;
 
     public ErrorResponse(String message, List<String> details) {
         super();
