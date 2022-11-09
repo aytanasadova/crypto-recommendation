3dff7363fbc60c35cb96f334c3f84f8c65d2d7c8
Aytam Asadova <aytanasadova1993@gmail.com>
2022-11-09 01:16:57 +0100

    Controller tests are done

--- src/test/java/xm/cryptorecommendation/CryptoDataControllerTest.java
+++ src/test/java/xm/cryptorecommendation/CryptoDataControllerTest.java
@@ -11,6 +11,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,13 +25,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class CryptoDataControllerTest {
     @Autowired
     private MockMvc mvc;
-    String basePath="/crypto-data/";
+    String basePath = "/crypto-data/";
 
     @Test
-    public void listAllWithOldestPrice() throws Exception
-    {
-        String uri =  basePath+"/withOldestPrice/";
-        mvc.perform( MockMvcRequestBuilders
+    public void listAllWithOldestPrice() throws Exception {
+        String uri = basePath + "/withOldestPrice/";
+        mvc.perform(MockMvcRequestBuilders
                         .get(uri)
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
@@ -34,14 +38,14 @@ public class CryptoDataControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].symbol").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].price").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].symbol").isNotEmpty())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[*].price").isNotEmpty());;
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[*].price").isNotEmpty());
+        ;
     }
 
     @Test
-    public void listAllWithNewestPrice() throws Exception
-    {
-        String uri =  basePath+"/withNewestPrice/";
-        mvc.perform( MockMvcRequestBuilders
+    public void listAllWithNewestPrice() throws Exception {
+        String uri = basePath + "/withNewestPrice/";
+        mvc.perform(MockMvcRequestBuilders
                         .get(uri)
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
@@ -49,16 +53,15 @@ public class CryptoDataControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].symbol").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].price").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].symbol").isNotEmpty())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[*].price").isNotEmpty());;
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[*].price").isNotEmpty());
+        ;
     }
 
 
-
     @Test
-    public void listAllWithMinPrice() throws Exception
-    {
-        String uri =  basePath+"/withMinPrice/";
-        mvc.perform( MockMvcRequestBuilders
+    public void listAllWithMinPrice() throws Exception {
+        String uri = basePath + "/withMinPrice/";
+        mvc.perform(MockMvcRequestBuilders
                         .get(uri)
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
@@ -66,15 +69,15 @@ public class CryptoDataControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].symbol").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].price").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].symbol").isNotEmpty())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[*].price").isNotEmpty());;
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[*].price").isNotEmpty());
+        ;
     }
 
 
     @Test
-    public void listAllWithMaxPrice() throws Exception
-    {
-        String uri =  basePath+"/withMaxPrice/";
-        mvc.perform( MockMvcRequestBuilders
+    public void listAllWithMaxPrice() throws Exception {
+        String uri = basePath + "/withMaxPrice/";
+        mvc.perform(MockMvcRequestBuilders
                         .get(uri)
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
@@ -82,15 +85,15 @@ public class CryptoDataControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].symbol").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].price").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].symbol").isNotEmpty())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[*].price").isNotEmpty());;
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[*].price").isNotEmpty());
+        ;
     }
 
 
     @Test
-    public void listAllWithNormalizedPrice() throws Exception
-    {
-        String uri =  basePath+"/withNormalizedPrice/";
-        mvc.perform( MockMvcRequestBuilders
+    public void listAllWithNormalizedPrice() throws Exception {
+        String uri = basePath + "/withNormalizedPrice/";
+        mvc.perform(MockMvcRequestBuilders
                         .get(uri)
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
@@ -98,15 +101,15 @@ public class CryptoDataControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].symbol").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].price").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].symbol").isNotEmpty())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[*].price").isNotEmpty());;
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[*].price").isNotEmpty());
+        ;
     }
 
 
     @Test
-    public void shouldSuccessGetMinPriceByCryptoName() throws Exception
-    {
-        String uri =  basePath+"/minPriceByCryptoName/{name}";
-        mvc.perform( MockMvcRequestBuilders
+    public void shouldSuccessGetMinPriceByCryptoName() throws Exception {
+        String uri = basePath + "/minPriceByCryptoName/{name}";
+        mvc.perform(MockMvcRequestBuilders
                         .get(uri, "BTC")
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
@@ -116,17 +119,97 @@ public class CryptoDataControllerTest {
 
 
     @Test
-    public void shouldFailGetMinPriceByCryptoName() throws Exception
-    {
-        String uri =  basePath+"/minPriceByCryptoName/{name}";
+    public void shouldFailGetMinPriceByCryptoName() throws Exception {
+        String uri = basePath + "/minPriceByCryptoName/{name}";
 
-        mvc.perform( MockMvcRequestBuilders
-                        .get(uri, "BTCd")
+        mvc.perform(MockMvcRequestBuilders
+                        .get(uri, "nonExistCrypto")
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isNoContent());
 
     }
 
+    @Test
+    public void shouldSuccessGetMaxPriceByCryptoName() throws Exception {
+        String uri = basePath + "/maxPriceByCryptoName/{name}";
+        mvc.perform(MockMvcRequestBuilders
+                        .get(uri, "BTC")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.symbol").value("BTC"));
+    }
 
+
+    @Test
+    public void shouldFailGetMaxPriceByCryptoName() throws Exception {
+        String uri = basePath + "/maxPriceByCryptoName/{name}";
+
+        mvc.perform(MockMvcRequestBuilders
+                        .get(uri, "nonExistCrypto")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+    }
+
+    @Test
+    public void shouldSuccessGetOldestPriceByCryptoName() throws Exception {
+        String uri = basePath + "/oldestPriceByCryptoName/{name}";
+        mvc.perform(MockMvcRequestBuilders
+                        .get(uri, "BTC")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.symbol").value("BTC"));
+    }
+
+
+    @Test
+    public void shouldFailGetOldestPriceByCryptoName() throws Exception {
+        String uri = basePath + "/oldestPriceByCryptoName/{name}";
+
+        mvc.perform(MockMvcRequestBuilders
+                        .get(uri, "nonExistCrypto")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+    }
+
+    @Test
+    public void shouldSuccessGetNewestPriceByCryptoName() throws Exception {
+        String uri = basePath + "/newestPriceByCryptoName/{name}";
+        mvc.perform(MockMvcRequestBuilders
+                        .get(uri, "BTC")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.symbol").value("BTC"));
+    }
+
+
+    @Test
+    public void shouldFailGetNewestPriceByCryptoName() throws Exception {
+        String uri = basePath + "/newestPriceByCryptoName/{name}";
+
+        mvc.perform(MockMvcRequestBuilders
+                        .get(uri, "nonExistCrypto")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+    }
+
+    @Test
+    public void shouldSuccessByMaxNormalizedPriceByDate() throws Exception {
+        String uri = basePath + "/byMaxNormalizedPrice/date/{date}";
+        mvc.perform(MockMvcRequestBuilders
+                        .get(uri, LocalDate.of(3000,12,12))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+    }
 }
